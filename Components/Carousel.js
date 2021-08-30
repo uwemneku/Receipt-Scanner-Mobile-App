4230a560557ace33ef261dcf185630271664b42e
uwem <uwemneku@gmail.com>
2021-08-30 01:40:40 +0100

    added animations to carousel

--- Components/Carousel.js
+++ Components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import {Dimensions, Image, StyleSheet, View } from 'react-native'
 import Animated, { Extrapolate, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
@@ -6,16 +6,27 @@ const W = Dimensions.get('screen').width
 
 const Carousel = ({children}) => {
     const scrollX = useSharedValue(0)
+    const scrollRef = useRef();
 
     const scrollHandler = useAnimatedScrollHandler({
         onScroll:(_, ctx) => {
             scrollX.value = _.contentOffset.x
         }
     })
+
+    useEffect(() => {
+        let index = 0
+        setInterval(() => {
+            index = index > children.length-2 ? 0: index+1 
+            scrollRef.current.scrollTo({x:W*index, y:0, animated:true})
+        }, 2000);
+    }, [])
+
     return (
         <View  style={styles.container} >
             <View>
                 <Animated.ScrollView
+                    ref={scrollRef}
                     horizontal={true}
                     snapToInterval={W}
                     decelerationRate='normal'
